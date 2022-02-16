b334157bdd940c0bb667e63e332ec92b02e51cff
Zio <60576791+Vyashni@users.noreply.github.com>
2022-02-16 10:12:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function checking()
+{
+  var ans=answer.value;
+  if (ans=="RUBEUS HAGRID")
+    {
+      document.getElementById("code").innerHTML=" Congrats, you got it right. Your code is VVVVV";
+}
+  else{
+    document.getElementById("code").innerHTML="Check your answer";
+  }
+  
+}
